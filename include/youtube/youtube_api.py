6027ebf7f247e2ad5933f2f63bb2fe0ab47705bc
safaacht <safaachtaoui@gmail.com>
2026-09-16 20:55:36 +0100

    Refactor extract_youtube_data function for improved readability and structure

--- include/youtube/youtube_api.py
+++ include/youtube/youtube_api.py
@@ -4,90 +4,93 @@ from googleapiclient.discovery import build
 import json
 from datetime import datetime
 
+def extract_youtube_data() :
+    
+    API_KEY = os.getenv("API_KEY")
+    CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
 
+    youtube = build("youtube" , "v3" , developerKey = API_KEY)
 
-youtube = build("youtube" , "v3" , developerKey = API_KEY)
-
-channel_request = youtube.channels().list(
-    part = "snippet , statistics , contentDetails" ,
-    forHandle = CHANNEL_HANDLE
-)
-
-channel_response = channel_request.execute()
-
-uploads_playlist_id = channel_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
-
-all_videos = []
-next_page_token = None
-
-while True :
-    video_request = youtube.playlistItems().list(
-        part = "snippet , contentDetails",
-        playlistId = uploads_playlist_id ,
-        maxResults = 50 ,
-        pageToken = next_page_token
+    channel_request = youtube.channels().list(
+        part = "snippet , statistics , contentDetails" ,
+        forHandle = CHANNEL_HANDLE
     )
 
-    video_response = video_request.execute()
+    channel_response = channel_request.execute()
 
-    all_videos.extend(video_response["items"])
-    next_page_token = video_response.get("nextPageToken")
+    uploads_playlist_id = channel_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 
-    if not next_page_token :
-        break
+    all_videos = []
+    next_page_token = None
 
+    while True :
+        video_request = youtube.playlistItems().list(
+            part = "snippet , contentDetails",
+            playlistId = uploads_playlist_id ,
+            maxResults = 50 ,
+            pageToken = next_page_token
+        )
 
-# print("Total videos extracted:", len(all_videos))
-# print("First video ID:", all_videos[0]["contentDetails"]["videoId"])
+        video_response = video_request.execute()
 
-video_ids = [
-    video["contentDetails"]["videoId"]
-    for video in all_videos
-]
+        all_videos.extend(video_response["items"])
+        next_page_token = video_response.get("nextPageToken")
 
-# print("Number of video IDs:", len(video_ids))
-
-all_details = []
-
-for i in range(0 , len(video_ids) , 50) :
-    batch_ids = video_ids[i:i+50]
-
-    details_request = youtube.videos().list(
-        part = "snippet , contentDetails , statistics" ,
-        id = ",".join(batch_ids)
-    )
+        if not next_page_token :
+            break
 
 
-    details_response = details_request.execute()
+    # print("Total videos extracted:", len(all_videos))
+    # print("First video ID:", all_videos[0]["contentDetails"]["videoId"])
 
-    all_details.extend(details_response["items"])
+    video_ids = [
+        video["contentDetails"]["videoId"]
+        for video in all_videos
+    ]
+
+    # print("Number of video IDs:", len(video_ids))
+
+    all_details = []
+
+    for i in range(0 , len(video_ids) , 50) :
+        batch_ids = video_ids[i:i+50]
+
+        details_request = youtube.videos().list(
+            part = "snippet , contentDetails , statistics" ,
+            id = ",".join(batch_ids)
+        )
 
 
-clean_videos = []
+        details_response = details_request.execute()
 
-for video in all_details:
-    clean_video = {
-        "videoId": video["id"],
-        "title": video["snippet"]["title"],
-        "publishedAt": video["snippet"]["publishedAt"],
-        "duration": video["contentDetails"]["duration"],
-        "viewCount": video["statistics"].get("viewCount", "0"),
-        "likeCount": video["statistics"].get("likeCount", "0"),
-        "commentCount": video["statistics"].get("commentCount", "0")
-    }
-
-    clean_videos.append(clean_video)
+        all_details.extend(details_response["items"])
 
 
+    clean_videos = []
 
-today = datetime.now().strftime("%Y-%m-%d")
+    for video in all_details:
+        clean_video = {
+            "videoId": video["id"],
+            "title": video["snippet"]["title"],
+            "publishedAt": video["snippet"]["publishedAt"],
+            "duration": video["contentDetails"]["duration"],
+            "viewCount": video["statistics"].get("viewCount", "0"),
+            "likeCount": video["statistics"].get("likeCount", "0"),
+            "commentCount": video["statistics"].get("commentCount", "0")
+        }
 
-ytb_json = f"/opt/airflow/data/youtube_SAFAA_{today}.json"
+        clean_videos.append(clean_video)
 
-with open(ytb_json , "w" , encoding= "utf-8") as file :
-    json.dump(clean_videos,file,ensure_ascii=False,indent=2)
 
-print("JSON saved :" , ytb_json)    
+
+    today = datetime.now().strftime("%Y-%m-%d")
+
+    ytb_json = f"/opt/airflow/data/youtube_SAFAA_{today}.json"
+
+    with open(ytb_json , "w" , encoding= "utf-8") as file :
+        json.dump(clean_videos,file,ensure_ascii=False,indent=2)
+
+    print("JSON saved :" , ytb_json) 
+
+    return ytb_json   
