274632e0b24ca747e0535a5046d8f6a196f9270a
safaacht <safaachtaoui@gmail.com>
2026-09-17 15:56:51 +0100

    Refactor load_to_staging and transform_to_core functions for improved structure and error handling

--- include/youtube/load_staging.py
+++ include/youtube/load_staging.py
@@ -1,39 +1,47 @@
 import json
 import psycopg2
 
-conn = psycopg2.connect(
-    host = "postgres",
-    port = 5432,
-    database = "youtube",
-    user = "postgres",
-    password = "postgres"
-)
+
+def load_to_staging() :
+
+    conn = psycopg2.connect(
+        host = "postgres",
+        port = 5432,
+        database = "youtube",
+        user = "postgres",
+        password = "postgres"
+    )
 
 
-with open("/opt/airflow/data/youtube_SAFAA_2026-09-15.json", "r", encoding="utf-8") as file:
-    videos = json.load(file)
+    with open("/opt/airflow/data/youtube_SAFAA_2026-09-15.json", "r", encoding="utf-8") as file:
+        videos = json.load(file)
 
 
-cursor = conn.cursor()
+    cursor = conn.cursor()
 
-for video in videos:
-    cursor.execute("""
-        INSERT INTO staging.youtube_videos
-        (video_id, title, published_at, duration, view_count, like_count, comment_count)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-        """, (
-        video["videoId"],
-        video["title"],
-        video["publishedAt"],
-        video["duration"],
-        video["viewCount"],
-        video["likeCount"],
-        video["commentCount"]
-    ))
+    for video in videos:
+        cursor.execute("""
+            INSERT INTO staging.youtube_videos
+            (video_id, title, published_at, duration, view_count, like_count, comment_count)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (video_id) DO NOTHING
+            """, (
+            video["videoId"],
+            video["title"],
+            video["publishedAt"],
+            video["duration"],
+            video["viewCount"],
+            video["likeCount"],
+            video["commentCount"]
+        ))
 
-conn.commit()
+    conn.commit()
 
-cursor.close()
-conn.close()
+    cursor.close()
+    conn.close()
 
-print("Videos inserted into Staging:", len(videos))    
+    print("Videos inserted into Staging:", len(videos))    
+
+    # return videos
+
+print(load_to_staging())
--- include/youtube/transform_core.py
+++ include/youtube/transform_core.py
@@ -1,91 +1,85 @@
 import psycopg2
 
+def transform_to_core():
 
-conn = psycopg2.connect(
-    host="postgres",
-    port=5432,
-    database="youtube",
-    user="postgres",
-    password="postgres"
-)
-
-# print("Connected to PostgreSQL!")
-
-cursor = conn.cursor()
-
-# ===== insertion ====
-
-cursor.execute("""
-    INSERT INTO core.youtube_videos (
-        video_id,
-        title,
-        published_at,
-        duration_seconds,
-        view_count,
-        like_count,
-        comment_count
+    conn = psycopg2.connect(
+        host="postgres",
+        port=5432,
+        database="youtube",
+        user="postgres",
+        password="postgres"
     )
-    SELECT
-        s.video_id,
-        s.title,
-        s.published_at::TIMESTAMP,
 
-        COALESCE((substring(s.duration FROM '(\d+)H'))::INTEGER, 0) * 3600
-        + COALESCE((substring(s.duration FROM '(\d+)M'))::INTEGER, 0) * 60
-        + COALESCE((substring(s.duration FROM '(\d+)S'))::INTEGER, 0),
+    cursor = conn.cursor()
 
-        s.view_count::BIGINT,
-        s.like_count::BIGINT,
-        s.comment_count::BIGINT
+    # ===== INSERT new videos =====
 
-    FROM staging.youtube_videos s
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM core.youtube_videos c
-        WHERE c.video_id = s.video_id
-    );
-""")
+    cursor.execute("""
+        INSERT INTO core.youtube_videos (
+            video_id,
+            title,
+            published_at,
+            duration_seconds,
+            view_count,
+            like_count,
+            comment_count
+        )
+        SELECT
+            s.video_id,
+            s.title,
+            s.published_at::TIMESTAMP,
 
-conn.commit()
+            COALESCE((substring(s.duration FROM '(\\d+)H'))::INTEGER, 0) * 3600
+            + COALESCE((substring(s.duration FROM '(\\d+)M'))::INTEGER, 0) * 60
+            + COALESCE((substring(s.duration FROM '(\\d+)S'))::INTEGER, 0),
 
-# print("Transformation completed!")
+            s.view_count::BIGINT,
+            s.like_count::BIGINT,
+            s.comment_count::BIGINT
 
-# ===update====
-
-cursor.execute("""
-    UPDATE core.youtube_videos c
-    SET
-        title = s.title,
-        published_at = s.published_at::TIMESTAMP,
-
-        duration_seconds =
-            COALESCE((substring(s.duration FROM '(\d+)H'))::INTEGER, 0) * 3600
-            + COALESCE((substring(s.duration FROM '(\d+)M'))::INTEGER, 0) * 60
-            + COALESCE((substring(s.duration FROM '(\d+)S'))::INTEGER, 0),
-
-        view_count = s.view_count::BIGINT,
-        like_count = s.like_count::BIGINT,
-        comment_count = s.comment_count::BIGINT
-
-    FROM staging.youtube_videos s
-    WHERE c.video_id = s.video_id;
-""")
-
-# ===supression===
-
-cursor.execute("""
-    DELETE FROM core.youtube_videos c
-    WHERE NOT EXISTS (
-        SELECT 1
         FROM staging.youtube_videos s
-        WHERE s.video_id = c.video_id
-    );
-""")
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM core.youtube_videos c
+            WHERE c.video_id = s.video_id
+        );
+    """)
 
-conn.commit()
+    # ===== UPDATE existing videos =====
 
-print("Update synchronization completed!")
-print("Delete synchronization completed!")
+    cursor.execute("""
+        UPDATE core.youtube_videos c
+        SET
+            title = s.title,
+            published_at = s.published_at::TIMESTAMP,
 
-cursor.close()
-conn.close()
+            duration_seconds =
+                COALESCE((substring(s.duration FROM '(\\d+)H'))::INTEGER, 0) * 3600
+                + COALESCE((substring(s.duration FROM '(\\d+)M'))::INTEGER, 0) * 60
+                + COALESCE((substring(s.duration FROM '(\\d+)S'))::INTEGER, 0),
+
+            view_count = s.view_count::BIGINT,
+            like_count = s.like_count::BIGINT,
+            comment_count = s.comment_count::BIGINT
+
+        FROM staging.youtube_videos s
+        WHERE c.video_id = s.video_id;
+    """)
+
+    # ===== DELETE videos no longer in staging =====
+
+    cursor.execute("""
+        DELETE FROM core.youtube_videos c
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM staging.youtube_videos s
+            WHERE s.video_id = c.video_id
+        );
+    """)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    print("Core synchronization completed!")
